90b9aefbaac421249e0aa372c81b1d84ce512505
dixy <dixympoutou@gmail.com>
2023-12-13 12:29:33 +0100

    index complete  done

--- views/body.php
+++ views/body.php
@@ -7,18 +7,18 @@ $ligne1 = 0; ?>
     <div class="row index-banner">
         <div class="display-1">
             <h1 class="text-white text-capitalize 
-            display-1 mt-5 text-align-center">bienvenue aux jeux olympique</h1>
+            display-1 mt-5 text-center">bienvenue aux jeux olympique</h1>
             
         </div>
     </div>
 
     <div class="row index-s1 ">
         <div class="col-md-12">
-            <h1 class="text-uppercase fw-light mt-4">  les prochaines rencontres  </h1>
+            <h1 class="text-uppercase fw-light mt-4 text-center">  les prochaines rencontres  </h1>
             <hr class="hr"/>
             <table class="table table-striped table-hover">
                     <thead>
-                        <tr class="text-capitalize">
+                        <tr class="text-capitalize text-center">
                             <th scope="col">#</th>
                             <th scope="col">equipe</th>
                             <th scope="col">vs </th>
@@ -30,7 +30,7 @@ $ligne1 = 0; ?>
 
                     <tbody>
                         <?php foreach (MatchesAvenir() as $value) : $ligne++ ?>
-                            <tr>
+                            <tr class="text-center">
                                 <th> <?= $ligne   ?>  </th>
                                 <td><?= $value["id_equipe_a"]   ?></td>
                                 <td>-</td>
@@ -42,38 +42,46 @@ $ligne1 = 0; ?>
 
                     </tbody>
             </table>
-            <form action="." method="Post">
+        
+
+            <form action="." method="Post" class="mt-5 text-center">
                 <button type="submit" class="btn btn-outline-danger" name="anciene">Ancienne Matches</button>
             </form>
+
             <?php if(isset($_POST['anciene'])) { ?>
-            <h1 class="text-uppercase fw-light">  les anciennes matches  </h1>
+            <h2 class="text-uppercase fw-light mb-2 mt-2 text-center">  les anciennes matches  </h2>
             <hr class="hr"/>
-            <table class="table table-striped table-hover">
-                    <thead>
-                        <tr class="text-capitalize">
-                            <th scope="col">#</th>
-                            <th scope="col">equipe</th>
-                            <th scope="col">vs </th>
-                            <th scope="col">equipe</th>
-                            <th scope="col">date </th>
-                            <th scope="col">lieu</th>
-                        </tr>
-                    </thead>
+            
 
-                    <tbody>
-                        <?php foreach (MatchesPasse() as $value) : $ligne1++ ?>
-                            <tr>
-                                <th> <?= $ligne1   ?>  </th>
-                                <td><?= $value["id_equipe_a"]   ?></td>
-                                <td>-</td>
-                                <td><?= $value["id_equipe_b"]   ?></td>
-                                <td><?= $value["date_de_rencontre"]   ?> </td>
-                                <td class="text-uppercase"><?= $value["lieu"]   ?></td>                               
-                            </tr>
+                  
+                    <?php foreach (MatchesPasse() as $value) : $ligne1++ ?>
+                        <table class="table table table-hover mb-5">
+                            <tbody>
+                                <tr class="text-center">
+                                    <td colspan="5"><?= $value["date_de_rencontre"] ?> </td>
+                                </tr>
+
+                                <tr class="text-center">
+                                    <td><?= $value["id_equipe_a"]   ?></td>
+                                    <td><strong><?= $value["score_equipe_a"]   ?></strong> </td>
+                                    <td class="text-center"><strong>-</strong></td>
+                                    <td> <strong><?= $value["score_equipe_b"]?></strong> </td>
+                                    <td><?= $value["id_equipe_b"]   ?></td>
+                                </tr>
+
+                                <tr class="text-center">
+                                    <td colspan="5"><?= $value["lieu"]   ?></td>
+                                </tr>
+                                <tr class="table text-center">
+                                    <td colspan="5"><?= $value["type"]   ?></td>
+                                </tr>                
+                            </tbody>
+                        </table>
                         <?php  endforeach  ?>
+                        
 
-                    </tbody>
-            </table>
+                    
+            
 
     
     <?php } ?>
